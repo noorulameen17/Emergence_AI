37e64e709359ca385ac973aeba0e2a73ca8b50e5
Noorul Ameen <noorulameen9220@gmail.com>
2024-12-08 11:27:41 +0530

    rebuilt

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  webpack: (config) => {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    }
+    return config
+  }
+}
+
+module.exports = nextConfig
