7334cd665f7ff8a6761f4b36694ac97e94073dc9
Noorul Ameen <noorulameen9220@gmail.com>
2024-12-22 21:58:12 +0530

    updated layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { ClerkProvider } from '@clerk/nextjs';
 import { Inter } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -7,7 +8,7 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>{children}<Analytics /> </body>
       </html>
     </ClerkProvider>
   );
